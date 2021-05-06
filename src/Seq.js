368f6867c2af3a3ddfc4e7bf969b3493e55dff9e
Csabi1998 <61312438+Csabi1998@users.noreply.github.com>
2021-05-06 18:34:02 +0200

    super fix

--- src/Seq.js
+++ src/Seq.js
@@ -164,7 +164,7 @@ Seq.prototype[IS_SEQ_SYMBOL] = true;
 
 export class ArraySeq extends IndexedSeq {
   constructor(array) {
-    let list = [];
+    let list = {};
     super(list);
     this._array = array;
     this.size = array.length;
